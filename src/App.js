99e51a864fef3ff0d50d694fdfe87e8d27eecbed
yulianaM <yulianamallorga@gmail.com>
2023-03-24 23:46:42 -0300

    modify the style of the Footer component

--- src/App.js
+++ src/App.js
@@ -12,7 +12,6 @@ function App() {
   return (
     <Router>
       <Navigation/>
-  
       <main>
         <Routes>
           <Route exact path='/' element={ <Home/> }/>
--- src/components/layout/footer/Footer.js
+++ src/components/layout/footer/Footer.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
 import { Link } from 'react-router-dom';
 import Logo from "../logo/Logo";
 import './Footer.css'
@@ -7,24 +9,36 @@ import './Footer.css'
 const Footer = () => {
     return (
         <div>
-            <footer className='footer-style'>
-                <Container>
-                    <div className='d-flex  justify-content-between  align-items-center p-3'>
-                        <Logo />
-                        <div className='d-flex text-center justify-content-around ms-auto w-25'>
-                            <div>
-                                <Link className="footer-link" to="/">Home</Link>
-                            </div>
-                            <div>
-                                <Link className="footer-link" to="/contact">Contacto</Link>
-                            </div>
-                        </div>
-                    </div>
-                   
-                </Container>   
+            <footer className='footer-style p-3 w-100'>
+                <Navbar variant="dark">
+                    <Container>
+                            <div className='row d-flex align-items-center justify-content-center'>
+                                <Navbar.Brand className='col-md-4 col-sm-6 col-lg-2 m-auto d-flex '>
+                                    <div className='m-auto '>
+                                        <Logo />
+                                    </div>
+                                </Navbar.Brand>
+                                    <Nav className="row d-flex  text-center col-md-8 col-sm-6 col-lg-10 justify-content-lg-between">
+                                        <Link className="nav-link col-sm-6 col-md-3 col-lg-2'" to="/">Home</Link>
+                                    <Link className="nav-link col-sm-6 col-md-3 col-lg-2" to="/register">Registrate</Link>
+                                    <Link className="nav-link col-sm-6 col-md-3 col-lg-2" to="*">Nosotros</Link>
+                                    <Link className="nav-link col-sm-6 col-md-3 col-lg-2" to="*">Ver más</Link>
+                                </Nav>
+                                 <div className='col-12 text-white mt-3'>
+                                <p className="text-center">
+                                        WorldSurveys. All rights reserved &copy;
+                                     </p>
+                                 </div> 
+                            </div>   
+                             
+                    </Container> 
+                </Navbar>  
             </footer>
         </div>
     );
 };
 
 export default Footer;
+
+
+  
