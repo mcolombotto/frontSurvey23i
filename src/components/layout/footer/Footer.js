bbe1a448de1795c2cda0c8572133336cffd9ba95
yulianaM <yulianamallorga@gmail.com>
2023-03-26 02:21:36 -0300

    fix footer  responsive

--- src/components/layout/footer/Footer.js
+++ src/components/layout/footer/Footer.js
@@ -12,20 +12,20 @@ const Footer = () => {
             <footer className='footer-style p-3 w-100'>
                 <Navbar variant="dark">
                     <Container>
-                            <div className='row d-flex align-items-center justify-content-center'>
+                        <div className='row w-100 align-items-center'>
                                 <Navbar.Brand className='col-md-4 col-sm-6 col-lg-2 m-auto d-flex '>
-                                    <div className='m-auto '>
+                                <div className='m-auto pe-4'>
                                         <Logo />
                                     </div>
                                 </Navbar.Brand>
                                     <Nav className="row d-flex  text-center col-md-8 col-sm-6 col-lg-10 justify-content-lg-between">
                                         <Link className="nav-link col-sm-6 col-md-3 col-lg-2'" to="/">Home</Link>
                                     <Link className="nav-link col-sm-6 col-md-3 col-lg-2" to="/register">Registrate</Link>
-                                    <Link className="nav-link col-sm-6 col-md-3 col-lg-2" to="*">Nosotros</Link>
-                                    <Link className="nav-link col-sm-6 col-md-3 col-lg-2" to="*">Ver más</Link>
+                                <Link className="nav-link col-sm-6 col-md-3 col-lg-2" to="*">Contacto</Link>
+                                <Link className="nav-link col-sm-6 col-md-3 col-lg-2" to="*">Nosotros</Link>
                                 </Nav>
                                  <div className='col-12 text-white mt-3'>
-                                <p className="text-center">
+                                <p className="text-center font">
                                         WorldSurveys. All rights reserved &copy;
                                      </p>
                                  </div> 
