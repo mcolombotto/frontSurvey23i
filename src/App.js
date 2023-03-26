e3be00fa8a8acf2fff2a47b607f9816ad3794aaf
yulianaM <yulianamallorga@gmail.com>
2023-03-26 02:02:17 -0300

    fix navbar responsive

--- src/App.js
+++ src/App.js
@@ -7,10 +7,6 @@ import Home from './components/views/home/Home.js'
 import Error404 from './components/views/error404/Error404'
 import Footer from './components/layout/footer/Footer';
 import Register from './components/views/register/Register'
-import Heading from './components/views/Heading';//TODO SACAR DESPUES
-import Section from './components/views/Section'; //TODO SACAR DESPUES
-//import Hijo from './components/layout/Hijo';
-//import { UserProvider } from "./components/layout/UserProvide";
 
 
 function App() {
@@ -18,25 +14,14 @@ function App() {
   return (
     <>
       <Router>
-        <Navigation>
-        </Navigation>
+        <Navigation/>
         <main>
-          {/* <UserProvider>
-            <div className='bg-danger'>
-              <Hijo />
-            </div>
-          </UserProvider> */}
           <Routes>
             <Route exact path='/' element={ <Home/> }/>
             <Route exact path='/register' element={ <Register/> }/>
             <Route exact path='*' element={<Error404/> }/>
           </Routes>
               
-          <Section level={4}>
-            <Heading>About</Heading>
-            <Heading>Photos</Heading>
-            <Heading>Videos</Heading>
-          </Section>
         </main>
         <Footer/>
       </Router>
