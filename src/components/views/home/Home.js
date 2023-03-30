11fcf429d5dedaeff5d885283c5758b8c73ba945
erickpipke <erickpipke@gmail.com>
2023-03-29 21:01:16 -0300

    addition of category buttons

--- src/components/views/home/Home.js
+++ src/components/views/home/Home.js
@@ -1,27 +1,13 @@
-import React, { useState } from "react";
+import React from "react";
 import Slider from './slider';
 import slides  from './formsDB.json'
 import CardList from './Cards.js';
-import formsData  from './formsDB.json'
-import Pagination from "./Pagination";
 
 const Home = () => {
-    const [currentPage, setCurrentPage] = useState(1);
-    const [postsPerPage] = useState(3);
-	const lastPostIndex = currentPage * postsPerPage;
-    const firstPostIndex = lastPostIndex - postsPerPage;
-    const currentPosts = formsData.slice(firstPostIndex, lastPostIndex);
-
     return (
     <>
         <Slider slides={slides}/>
-        <CardList cards= {currentPosts}/>
-		<Pagination
-                totalPosts={formsData.length}
-                postsPerPage={postsPerPage}
-                setCurrentPage={setCurrentPage}
-                currentPage={currentPage}
-            />
+        <CardList cards= {slides}/>
     </>
     );
 };
