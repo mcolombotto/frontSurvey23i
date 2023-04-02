85a6af2074bc235545101defbe439c3db83974c1
mcolombotto <mcolombotto@gmail.com>
2023-04-02 20:28:42 -0300

    footer added

--- src/components/layout/footer/Footer.js
+++ src/components/layout/footer/Footer.js
@@ -1,9 +1,17 @@
 import React from 'react';
+import { Navbar, Container } from 'react-bootstrap';
+
 
 const Footer = () => {
     return (
         <div>
-            
+            <footer className='footer-style p-3 w-100'>
+                <Navbar variant="dark">
+                    <Container>
+                           
+                    </Container> 
+                </Navbar>  
+            </footer>
         </div>
     );
 };
