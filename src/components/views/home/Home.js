698a1da3b3d4cdee27030f6840b49c4105b5a68b
yulianaM <yulianamallorga@gmail.com>
2023-04-04 01:57:49 -0300

    Delete the console error when following logo192 and logo512

--- src/components/views/home/Home.js
+++ src/components/views/home/Home.js
@@ -2,9 +2,9 @@ import React from 'react';
 import "./Home.css";
 const Home = () => {
     return (
-        <div className='Home'>
-            <h1 className='text-center'>Hola</h1>
-        </div>
+        <>
+            
+        </>
     );
 };
 
