beaa4c52a5f6e22f3a9ae1a21b72a91c93135ea2
mcolombotto <mcolombotto@gmail.com>
2023-04-04 16:59:50 -0300

    changes on register, footer and error404

--- src/components/views/register/Register.js
+++ src/components/views/register/Register.js
@@ -1,4 +1,4 @@
-import React, { useState, useContext } from 'react';
+import React, { useState, useContext, useEffect} from 'react';
 import UserContext from '../../layout/context/UserContext';
 import { useForm } from "react-hook-form";
 import { Container, Form } from "react-bootstrap";
@@ -7,42 +7,41 @@ import { FaEye } from "react-icons/fa"
 import { FaEyeSlash } from "react-icons/fa"
 import Swal from 'sweetalert2';
 import { useNavigate } from 'react-router-dom';
-import axios from 'axios';
 
 const Register = () => {
     const { register, handleSubmit, reset, formState: { errors } } = useForm();
 
     const [errorPassword, setErrorPassword] = useState(false);
-    const [username, setUsername] = useState('')
-    const [password, setPassword] = useState('')
-    const [email, setEmail] = useState('')
     const [typeInput, setTypeInput] = useState('password')
 
     const context = useContext(UserContext);
     const navigate = useNavigate()
-
+    //variable de entorno
     const URL = process.env.REACT_APP_API_SURVEYS_USER;
 
-    const onSubmit = data => {
+    const onSubmit = async data => {
         setErrorPassword(false);
     
         if (data.password1 !== data.password2) {
             setErrorPassword(true);
         } else {
-            fetch('https://randomuser.me/api/')
-                .then((resp) => resp.json())
-                .then(async (data) => {
-                    const myUser = {
-                        username: username,
-                        email: email,
-                        password: password,
-                        uuid: data.results[0].login.uuid,
-                        img: data.results[0].picture.large,
-                        role:'user_role',
-                        estate: true
-                    } 
-                    context.setUser(myUser)
-                    
+            try {
+                const res = await fetch(URL, {
+                    method: 'POST',
+                    headers: {
+                        'content-type': 'application/json'
+                    },
+                    body: JSON.stringify({ 
+                        username: data.name,
+                        email: data.email,
+                        password: data.password1
+                    }),
+                });
+
+                const response = await res.json();
+                const { token, uid, userName, message } = response;
+
+                if (res.status === 200) {
                     reset(formValues => ({
                         ...formValues,
                         name: '',
@@ -50,115 +49,130 @@ const Register = () => {
                         password1: '',
                         password2: '',
                     }));
-
-                    if(context.user){
-                        try {
-                            const res = await axios.post(
-                                URL,
-                                context.user);
-                            const info = await res.json();
-                            console.log('info', info);
-                            Swal.fire(`Tu registro fue exitoso`)
-                            setTimeout(() => { navigate("/") }, 2500)
-                        } catch (error) {
-                            console.log(error);
-                            alert('Ocurrió un problema, vuelve a intentarlo.')
-                        }
-                    };
-                  
                     
+                    localStorage.setItem('token', token);
 
+                    context.setUser({
+                        uuid: uid,
+                        username: userName,
+                        email: data.email,
+                        estate: true,
+                        img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRUJt3kjJp8q750SzS-kr7cMITugGpEh-Vdq0NeWS4&s'
+                    });
+
+                    Swal.fire(message);
+                    return setTimeout(() => { navigate("/") }, 2500)
+                } 
+
+                if (res.status === 400) {
+                    Swal.fire({
+                        icon: 'error',
+                        title: 'Oops...',
+                        text: message,
+                    })
+                } else {
+                    Swal.fire({
+                        icon: 'error',
+                        title: 'Oops...',
+                        text: 'Ocurrio un problema, vuelve a intentarlo!',
+                    })
+                };
+            } catch (error) {
+                Swal.fire({
+                    icon: 'error',
+                    title: 'Oops...',
+                    text: 'El envío de datos falló!',
                 })
-                .catch(error => console.error(error))    
-        }
+            }
+       };
     }
       
     return (
-        <Container className="py-5">
-            <div className='form-container row mainRegister'>
-                <form className="my-5 form col-sm-8 col-lg-6" onSubmit={handleSubmit(onSubmit)}>
-                    <div className='mb-3'>
-                        <h1 className='text-center'>Registro</h1>
-                    </div>
-                    <div className='mb-3'>
-                        <Form.Label>Nombre de usuario<span className='text-danger font-weight-bold'>*</span> </Form.Label>
-                        <input 
-                            className="form-control " 
-                            placeholder="Ej: John Perez"
-                            onBlurCapture={(e) => setUsername(e.target.value)}
-                            {...register("name", { required: true, maxLength: 100, pattern: /^[A-Za-z\s?]+$/ })}
-                        />
-                       
-                        {errors.name && errors.name.type === 'required' && <span className='error'>Este campo es requerido. </span>}
-                        {errors.name && errors.name.type === 'maxLength' && <span className='error'>Este campo tiene un máximo de 60 </span>}
-                        {errors.name && errors.name.type === 'pattern' && <span className='error'>En este campo sólo puedes ingresar letras</span>}
-                        
-                    </div>
-                    <div className='mb-3'>
-                        <Form.Label>Email<span className='text-danger font-weight-bold'>*</span></Form.Label>
-                        <input 
-                            className="form-control " 
-                            type='email'
-                            placeholder="Ej: John_Perez@email.com"
-                            onBlurCapture={(e) => setEmail(e.target.value)}
-                            {...register("email", { required: true, maxLength: 100, pattern: /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/ })} 
-                        />
-                        {errors.email && errors.email.type === 'required' && <span className='error'>Este campo es requerido. </span>}
-                        {errors.email && errors.email.type === 'maxLength' && <span className='error'>Este campo tiene un máximo de 60 caracteres</span>}
-                        {errors.email && errors.email.type === 'pattern' && <span className='error'> El correo tiene un formato incorrecto. Formato esperado: email@email.com </span>}
-                    </div>
-                    <div className='mb-3 position-relative'>
-                        <Form.Label>Contraseña<span className='text-danger font-weight-bold'>*</span></Form.Label>
-                        <input 
-                            className="form-control " 
-                            type={typeInput}
-                            onBlurCapture={(e)=>setPassword(e.target.value)}
-                            {...register("password1", { required: true, maxLength: 100, pattern: /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[$@$!%*?&])[A-Za-z\d$@$!%*?&]{8,90}/ })} 
-                        />
-                        <button
-                            type="button"
-                            className='position-absolute buttonPosition btn'
-                            onClick={() => { typeInput === 'password' ? setTypeInput('text') : setTypeInput('password') }}
-                        > 
-                            {typeInput === 'password' ? <FaEyeSlash /> : < FaEye />}
-                        </button>
-                        {errors.password1 && errors.password1.type === 'required' && <span className='error'>Este campo es requerido. </span>}
-                        {errors.password1 && errors.password1.type === 'maxLength' && <span className='error'> Este campo tiene un máximo de 90 caracteres</span>}
-                        {errors.password1 && errors.password1.type === 'pattern' && (
-                            <span className='error'>
-                                La contraseña no es válida. Esta debe tener mínimo 8 caracteres, al menos una letra mayúscula,
-                                al menos una letra minúscula y al menos un caracter especial (@$!%*?&)
-                            </span>
-                        )}
-                    </div>
-                    <div className='mb-3 position-relative'>
-                        <Form.Label>Repetir contraseña<span className='text-danger font-weight-bold'>*</span></Form.Label>
-                        <input 
-                            className="form-control " 
-                            type={typeInput}
-                            {...register("password2", { required: true, maxLength: 90, pattern: /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[$@$!%*?&])([A-Za-z\d$@$!%*?&]|[^ ]){8,90}$/ })} 
-                        />
-                        {errors.password2 && errors.password2.type === 'required' && <span className='error'>Este campo es requerido. </span>}
-                        {errors.password2 && errors.password2.type === 'maxLength' && <span className='error'>Este campo tiene un máximo de 90 caracteres</span>}
-                        {errors.password2 && errors.password2.type === 'pattern' && <span className='error'>La contraseña no es válida. Esta debe tener mínimo 8 caracteres,
-                            al menos una letra mayúscula, al menos una letra minúscula y al menos un caracter especial (@$!%*?&)</span>}
-                        {errorPassword && <span className='error'>Las contraseñas no coinciden</span>}
-                        <button
-                            type="button"
-                            className='position-absolute buttonPosition btn'
-                            onClick={() => { typeInput === 'password' ? setTypeInput('text') : setTypeInput('password') }}
-                        >
-                            {typeInput === 'password' ? <FaEyeSlash /> : < FaEye />}
-                        </button>
-                    </div>
-                    <div>
-                        <button className="btn btn-danger" type="submit"> Enviar</button>
-                    </div>
-                </form>
-            </div>
-        </Container >
+        <div className='mainRegister'>
+            <Container className="py-5 ">
+                <div className='form-container row justify-content-center'>
+                    <form className="my-5 form col-sm-8 col-lg-6" onSubmit={handleSubmit(onSubmit)}>
+                        <div className='mb-3'>
+                            <h1 className='text-center'>Registro</h1>
+                        </div>
+                        <div className='mb-3'>
+                            <Form.Label>Nombre de usuario<span className='text-danger font-weight-bold'>*</span> </Form.Label>
+                            <input
+                                className="form-control "
+                                placeholder="Ej: John Perez"
+                                {...register("name", { required: true, maxLength: 100, pattern: /^[A-Za-z\s?]+$/ })}
+                            />
+
+                            {errors.name && errors.name.type === 'required' && <span className='error'>Este campo es requerido. </span>}
+                            {errors.name && errors.name.type === 'maxLength' && <span className='error'>Este campo tiene un maximo de 60 </span>}
+                            {errors.name && errors.name.type === 'pattern' && <span className='error'>En este campo solo puedes ingresar letras</span>}
+
+                        </div>
+                        <div className='mb-3'>
+                            <Form.Label>Email<span className='text-danger font-weight-bold'>*</span></Form.Label>
+                            <input
+                                className="form-control "
+                                type='email'
+                                placeholder="Ej: John_Perez@email.com"
+                                {...register("email", { required: true, maxLength: 100, pattern: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/ })}
+                            />
+                            {errors.email && errors.email.type === 'required' && <span className='error'>Este campo es requerido. </span>}
+                            {errors.email && errors.email.type === 'maxLength' && <span className='error'>Este campo tiene un maximo de 60 caracteres</span>}
+                            {errors.email && errors.email.type === 'pattern' && <span className='error'> El correo tiene un formato incorrecto. Formato esperado: email@email.com </span>}
+                        </div>
+                        <div className='mb-3 position-relative'>
+                            <Form.Label>Contraseña<span className='text-danger font-weight-bold'>*</span></Form.Label>
+                            <input
+                                className="form-control "
+                                type={typeInput}
+                                {...register("password1", { required: true, maxLength: 100, pattern: /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[$@$!%*?&])[A-Za-z\d$@$!%*?&]{8,90}/ })}
+                            />
+                            <button
+                                type="button"
+                                className='position-absolute  buttonPosition btn'
+                                onClick={() => { typeInput === 'password' ? setTypeInput('text') : setTypeInput('password') }}
+                            >
+                                {typeInput === 'password' ? <FaEyeSlash /> : < FaEye />}
+                            </button>
+                            {errors.password1 && errors.password1.type === 'required' && <span className='error'>Este campo es requerido. </span>}
+                            {errors.password1 && errors.password1.type === 'maxLength' && <span className='error'> Este campo tiene un maximo de 90 caracteres</span>}
+                            {errors.password1 && errors.password1.type === 'pattern' && (
+                                <span className='error'>
+                                    La contraseña no es valida. Esta debe tener mìnimo 8 caracteres, al menos una letra mayúscula,
+                                    al menos una letra minucula y al menos un caracter especial (@$!%*?&)
+                                </span>
+                            )}
+                        </div>
+                        <div className='mb-3 position-relative'>
+                            <Form.Label>Repetir contraseña<span className='text-danger font-weight-bold'>*</span></Form.Label>
+                            <input
+                                className="form-control "
+                                type={typeInput}
+                                {...register("password2", { required: true, maxLength: 100, pattern: /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[$@$!%*?&])([A-Za-z\d$@$!%*?&]|[^ ]){8,90}$/ })}
+                            />
+                            {errors.password2 && errors.password2.type === 'required' && <span className='error'>Este campo es requerido. </span>}
+                            {errors.password2 && errors.password2.type === 'maxLength' && <span className='error'>Este campo tiene un maximo de 90 caracteres</span>}
+                            {errors.password2 && errors.password2.type === 'pattern' && <span className='error'>La contraseña no es valida. Esta debe tener mìnimo 8 caracteres,
+                                al menos una letra mayúscula, al menos una letra minucula y al menos un caracter especial (@$!%*?&)</span>}
+                            {errorPassword && <span className='error'>Las contraseñas no coinciden</span>}
+                            <button
+                                type="button"
+                                className='position-absolute buttonPosition btn'
+                                onClick={() => { typeInput === 'password' ? setTypeInput('text') : setTypeInput('password') }}
+                            >
+                                {typeInput === 'password' ? <FaEyeSlash /> : < FaEye />}
+                            </button>
+                        </div>
+                        <div>
+                            <button className="btn btn-danger" type="submit"> Enviar</button>
+                        </div>
+                    </form>
+                </div>
+            </Container >    
+        </div>
+        
        
-    );
+    )
 }
 
 export default Register
