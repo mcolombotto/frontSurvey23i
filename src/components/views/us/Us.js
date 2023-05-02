dd3c6460f6490f214d3e688bfcd7daa2bf9c5e46
yulianaM <yulianamallorga@gmail.com>
2023-05-02 17:38:17 -0300

    change card style

--- src/components/views/us/Us.js
+++ src/components/views/us/Us.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import imgWomen from "./../../../assets/img/us/women-office.jpg";
 import imgOffice from "./../../../assets/img/us/black-man-office.jpg";
 import yuliana from "../../../assets/img/us/yuliana.png"
@@ -12,7 +13,7 @@ function Us() {
         <section className="row p-2 text-center text-secondary">
             <div className="col-sm-12 col-md-6 col-lg-6 p-4">
                 <h1 className="p-1 diplay-1">WorldSurveys</h1>
-                    <p className="letter-first space-lette">
+                  <p className="letter-first space-letter">
                         Es una empresa de gestión de experiencias ágil creada para lo que
                         viene Brindamos soluciones intuitivas y centradas en las personas
                         que ayudan a los líderes de la industria a tomar decisiones
@@ -42,7 +43,7 @@ function Us() {
             </div>
             <div className="col-sm-12 col-md-6 col-lg-6">
                 <h1 className="p-1 diplay-1">Nuestras creencias y principios rectores</h1>
-                    <p className="letter-first space-lette">
+                  <p className="letter-first space-letter">
                         Nuestra visión, misión y valores reflejan los mejores intereses de
                         nuestros clientes, empleados y comunidad. Ellos inspiran nuestro
                         crecimiento. Ayudan a dar forma a nuestras decisiones comerciales,
@@ -52,70 +53,69 @@ function Us() {
                     </p>
             </div>
         </section >
-        <section className="container-fluid bg-new">
+        <section className="container-fluid">
               <div className="container">
                   <div className="row text-center py-5">
                       <h2 className="diplay-2 text-center text-light my-5 fst-italic">Conoce a algunos miembros de nuestro equipo</h2>
-                      <div className="col-lg-3 col-sm-6">
-                          <div className="card bg-gradient bg-light bg-opacity-10 border border-0 mb-3 card-wrapper">
+                      <div className="col-lg-3 col-md-6 col-sm-12">
+                          <div className="bg-gradient bg-light bg-opacity-10 border mb-3 card-wrapper">
                               <div className="text-bg-light pt-3">
                                   <figure className="w-75 mx-auto  rounded contenedor-img">
                                       <img src={luciano} className="img-fluid imagen-filter" alt="Luciano Dannert" />
                                   </figure>
                               </div>
-                              <div className="card-body text-light pb-3 fuente">
+                              <div className="card-body text-light pb-3">
                                   <h4 className="card-title display-5">Luciano</h4>
                                   <h5 className="mt-2">"Lider de equipo"</h5>
-                                  <a type="button" className="btn btn-primary" to="/error">Ver más</a>
+                                  <Link type="button" className="btn btn-primary" to="/error">Ver más</Link>
                               </div>
                           </div>
                       </div>
 
-                      <div className="col-lg-3 col-sm-6">
-                          <div className="card bg-gradient bg-light bg-opacity-10 border border-0 mb-3 card-wrapper">
+                      <div className="col-lg-3 col-md-6 col-sm-12">
+                          <div className="bg-gradient bg-light bg-opacity-10 border mb-3 card-wrapper">
                               <div className="text-bg-light pt-3">
-                                  <figure className="width-img-yuli mx-auto  rounded contenedor-img">
+                                  <figure className="w-75 mx-auto  rounded contenedor-img">
                                       <img src={yuliana} className="img-fluid imagen-filter" alt="Yuliana Mallorga"/>
                                   </figure>
                               </div>
-                              <div className="card-body text-light pb-3 fuente">
+                              <div className="card-body text-light pb-3">
                                   <h4 className="card-title display-5">Yuliana</h4>
                                   <h5 className="mt-2">"Desarrolladora web"</h5>
-                                  <a type="button" className="btn btn-primary" to="/error">Ver más</a>
+                                  <Link type="button" className="btn btn-primary" to="/error">Ver más</Link>
                               </div>
                           </div>
                       </div>
                       
-                      <div className="col-lg-3 col-sm-6">
-                          <div className="card bg-gradient bg-light bg-opacity-10 border border-0 mb-3 card-wrapper">
+                      <div className="col-lg-3 col-md-6 col-sm-12">
+                          <div className="bg-gradient bg-light bg-opacity-10 border mb-3 card-wrapper">
                               <div className="text-bg-light pt-3">
                                   <figure className="w-75 mx-auto  rounded contenedor-img">
                                       <img src={mariano} className="img-fluid imagen-filter" alt="Mariano Colombotto" />
                                   </figure>
                               </div>
-                              <div className="card-body text-light pb-3 fuente">
+                              <div className="card-body text-light pb-3">
                                   <h4 className="card-title display-5">Mariano</h4>
                                   <h5 className="mt-2">"Desarrollador web"</h5>
-                                  <a type="button" className="btn btn-primary" to="/error">Ver más</a>
+                                  <Link type="button" className="btn btn-primary" to="/error">Ver más</Link>
                               </div>
                           </div>
                       </div>
-                      <div className="col-lg-3 col-sm-6">
-                          <div className="card bg-gradient bg-light bg-opacity-10 border border-0 mb-3 card-wrapper">
+                      <div className="col-lg-3 col-md-6 col-sm-12">
+                          <div className="bg-gradient bg-light bg-opacity-10 border mb-3 card-wrapper">
                               <div className="text-bg-light pt-3">
-                                  <figure className=" mx-auto  width-img-paul rounded contenedor-img">
+                                  <figure className="w-75 mx-auto  rounded contenedor-img">
                                       <img src={erick} className="img-fluid imagen-filter" alt="Erick Pipke"/>
                                   </figure>
                               </div>
-                              <div className="card-body text-light pb-3 fuente">
+                              <div className="card-body text-light pb-3">
                                   <h4 className="card-title display-5">Erick</h4>
                                   <h5 className="mt-2">"Desarrollador web"</h5>
-                                  <a type="button" className="btn btn-primary" to="/error">Ver más</a>
+                                  <Link type="button" className="btn btn-primary" to="/error">Ver más</Link>
                               </div>
                           </div>
                       </div>
                   </div>
-
               </div>
           </section>
     </>
