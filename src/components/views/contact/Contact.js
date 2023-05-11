12d5ad381490b104061aebc74b47a785c73ec4e7
lucianodanert <lucianodanert@gmail.com>
2023-05-10 22:01:41 -0300

    contact request sent to mail

--- src/components/views/contact/Contact.js
+++ src/components/views/contact/Contact.js
@@ -3,172 +3,121 @@ import { useForm } from "react-hook-form";
 import { Container, Form } from "react-bootstrap";
 import "./Contact.css";
 import Swal from "sweetalert2";
+import emailjs from "@emailjs/browser";
+import { useRef } from "react";
+import { useNavigate } from "react-router-dom";
 
 const Contact = () => {
-    const {
-        register,
-        handleSubmit,
-        reset,
-        formState: { errors },
-    } = useForm();
+  const form = useRef();
 
-    const [message, setMessage] = useState("");
+  const [message, setMessage] = useState("");
+  const navigate = useNavigate();
+  const sendEmail = (e) => {
+    e.preventDefault();
 
-    const URL = process.env.REACT_APP_API_SURVEYS_CONTACT || '';
-
-    const onSubmit = async (data) => {
-        try {
-            const res = await fetch(URL, {
-                method: "POST",
-                headers: {
-                    "content-type": "application/json",
-                },
-                body: JSON.stringify({
-                    name: data.name,
-                    email: data.email,
-                    message: data.message
-                }),
-            });
-
-            if (res.status === 200) {
-                reset((formValues) => ({
-                    ...formValues,
-                    name: "",
-                    email: "",
-                    message: "",
-                }));
-
-                Swal.fire({
-                    text: message,
-                    icon: "success",
-                    color: "#fff",
-                    background: "#000",
-                    confirmButtonColor: "#3085d6",
-                });
-            } else {
-                Swal.fire({
-                    icon: "error",
-                    title: "Oops...",
-                    text: "Ha ocurrido un error al enviar el formulario. Intenta nuevamente.",
-                });
-            }
-        } catch (error) {
-            Swal.fire({
-                icon: "error",
-                title: "Oops...",
-                text: "Ha ocurrido un error al enviar el formulario. Intenta nuevamente.",
-            });
+    emailjs
+      .sendForm(
+        process.env.REACT_APP_EMAILJS_SERVICE_ID_CONTACT,
+        process.env.REACT_APP_EMAILJS_TEMPLATE_ID_CONTACT,
+        form.current,
+        process.env.REACT_APP_EMAILJS_PUBLIC_KEY
+      )
+      .then(
+        (result) => {
+          Swal.fire({
+            text: "Tu consulta fue enviada con exito",
+            icon: "success",
+            color: "#fff",
+            background: "#000",
+            confirmButtonColor: "#3085d6",
+          });
+          navigate("/");
+        },
+        (error) => {
+          Swal.fire({
+            icon: "error",
+            title: "Oops...",
+            text: "Ha ocurrido un error al enviar el formulario. Intenta nuevamente.",
+          });
         }
-    };
-   
-    return (
-        <div className="contact">
-            <Container className="py-5 ">
-                <div className="row justify-content-center">
-                    <div className="mb-3 text-light text-center">
-                        <h1>Contacto</h1>
-                        <h2 className="mt-4 fst-italic">
-                            Si deseas contactarte con "WorldSurvey" rellena este formulario.
-                        </h2>
-                    </div>
-                    <form
-                        className="my-5 form col-sm-8 col-lg-6"
-                        onSubmit={handleSubmit(onSubmit)}
-                    >
-                        <div className="mb-3">
-                            <Form.Label>
-                                Ingresa tu nombre
-                                <span className="text-danger font-weight-bold">*</span>{" "}
-                            </Form.Label>
-                            <input
-                                className="form-control "
-                                placeholder="Ej: Juan Perez"
-                                maxLength={100}
-                                {...register("name", {
-                                    required: true,
-                                    maxLength: 100,
-                                    pattern: /^[A-Za-z\s?]+$/,
-                                })}
-                            />
+      );
+  };
 
-                            {errors.name && errors.name.type === "required" && (
-                                <span className="error">Este campo es requerido. </span>
-                            )}
-                            {errors.name && errors.name.type === "maxLength" && (
-                                <span className="error">Este campo tiene un máximo de 100 letras</span>
-                            )}
-                            {errors.name && errors.name.type === "pattern" && (
-                                <span className="error">
-                                    En este campo solo puedes ingresar letras.
-                                </span>
-                            )}
-                        </div>
-                        <div className="mb-3">
-                            <Form.Label>
-                                Ingresa tu email
-                                <span className="text-danger font-weight-bold">*</span>
-                            </Form.Label>
-                            <input
-                                className="form-control "
-                                type="email"
-                                placeholder="Ej: John_Perez@email.com"
-                                maxLength={100}
-                                {...register("email", {
-                                    required: true,
-                                    maxLength: 100,
-                                    pattern: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/,
-                                })}
-                            />
-                            {errors.email && errors.email.type === "required" && (
-                                <span className="error">Este campo es requerido. </span>
-                            )}
-                            {errors.email && errors.email.type === "maxLength" && (
-                                <span className="error">
-                                    Este campo tiene un máximo de 100 caracteres.
-                                </span>
-                            )}
-                            {errors.email && errors.email.type === "pattern" && (
-                                <span className="error">
-                                    El correo tiene un formato incorrecto. Formato esperado:
-                                    email@email.com{" "}
-                                </span>
-                            )}
-                        </div>
-                        <div className="mb-3 position-relative d-flex flex-column">
-                            <Form.Label>
-                                Ingresa tu mensaje
-                                <span className="text-danger font-weight-bold">*</span>
-                            </Form.Label>
-                            <textarea
-                                className="form-control flex-column form-control-lg"
-                                maxLength={550}
-                                rows={7}
-                                onChangeCapture ={(e) => setMessage(e.target.value)}
-                                {...register("message", {
-                                    required: true,
-                                })}
-                            >
-                            </textarea>
-                           
-                            {errors.message && errors.message.type === "required" && (
-                                <span className="error">Este campo es requerido. </span>
-                            )}
-                            <div class="alert alert-warning mt-1">
-                                <p>
-                                    Te quedan {550 - message?.length} caracteres para utilizar en tu mensaje.
-                                </p>
-                            </div>
-                        </div>
-                        <div>
-                            <button className="btn btn-primary" type="submit">
-                                Enviar
-                            </button>
-                        </div>
-                    </form>
-                </div>
-            </Container>
+  return (
+    <div className="contact">
+      <Container className="py-5 ">
+        <div className="row justify-content-center">
+          <div className="mb-3 text-light text-center">
+            <h1>Contacto</h1>
+            <h2 className="mt-4 fst-italic">
+              Si deseas contactarte con "WorldSurvey" rellena este formulario.
+            </h2>
+          </div>
+          <form
+            ref={form}
+            onSubmit={sendEmail}
+            className="my-5 form col-sm-8 col-lg-6"
+          >
+            <div className="mb-3">
+              <Form.Label>
+                Ingresa tu nombre
+                <span className="text-danger font-weight-bold">*</span>{" "}
+              </Form.Label>
+              <input
+                className="form-control "
+                placeholder="Ej: Juan Perez"
+                maxLength={100}
+                name="user_name"
+                required = "true"
+              />
+            </div>
+            <div className="mb-3">
+              <Form.Label>
+                Ingresa tu email
+                <span className="text-danger font-weight-bold">*</span>
+              </Form.Label>
+              <input
+                className="form-control "
+                type="email"
+                placeholder="Ej: John_Perez@email.com"
+                maxLength={100}
+                name="user_email"
+                pattern= "^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$"
+                required = "true"
+              />
+            </div>
+            <div className="mb-3 position-relative d-flex flex-column">
+              <Form.Label>
+                Ingresa tu mensaje
+                <span className="text-danger font-weight-bold">*</span>
+              </Form.Label>
+              <textarea
+                className="form-control flex-column form-control-lg"
+                maxLength={550}
+                rows={7}
+                name="message"
+                required = "true"
+                onChangeCapture={(e) => setMessage(e.target.value)}
+                
+              ></textarea>
+
+              <div class="alert alert-warning mt-1">
+                <p>
+                  Te quedan {550 - message?.length} caracteres para utilizar en
+                  tu mensaje.
+                </p>
+              </div>
+            </div>
+            <div>
+              <button className="btn btn-primary" type="submit">
+                Enviar
+              </button>
+            </div>
+          </form>
         </div>
-    );
+      </Container>
+    </div>
+  );
 };
 
 export default Contact;
